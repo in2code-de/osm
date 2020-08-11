eabfadaa731c8e740b0e36ec29153131fe73e3f7
Alex Kellner <alexander.kellner@in2code.de>
2020-08-11 10:30:41 +0200

    [TASK] initial commit

---
+++ ext_emconf.php
@@ -0,0 +1,19 @@
+<?php
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'osm - OpenStreetMap',
+    'description' => 'A smart OpenStreetMap solution in TYPO3',
+    'category' => 'plugin',
+    'version' => '1.0.0',
+    'author' => 'Alex Kellner',
+    'author_email' => 'alexander.kellner@in2code.de',
+    'author_company' => 'in2code.de',
+    'state' => 'stable',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '10.4.0-10.4.99',
+            'php' => '7.2.0-7.99.99'
+        ],
+        'conflicts' => [],
+        'suggests' => [],
+    ]
+];
