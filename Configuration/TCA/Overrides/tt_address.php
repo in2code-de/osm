21178c38088ee22f61066932eec166cc074ea341
Andreas Nedbal <andreas.nedbal@in2code.de>
2025-01-14 14:19:13 +0100

    [BUGFIX] Check for loaded tt_address in TCA override

--- Configuration/TCA/Overrides/tt_address.php
+++ Configuration/TCA/Overrides/tt_address.php
@@ -1,8 +1,11 @@
 <?php
+
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+
 if (!defined('TYPO3')) {
     die('Access denied.');
 }
 
-if (($GLOBALS['TCA']['tt_address']['columns']['description']['config']['enableRichtext'] ?? null) !== null) {
+if (ExtensionManagementUtility::isLoaded('tt_address')) {
     $GLOBALS['TCA']['tt_address']['columns']['description']['config']['enableRichtext'] = false;
 }
