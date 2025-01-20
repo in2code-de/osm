8eccbda640227af3036cb93e31413fec731f1e1c
Andreas Nedbal <andreas.nedbal@in2code.de>
2025-01-20 15:10:44 +0100

    [TASK] Raise extension version and TYPO3 constraints

--- ext_emconf.php
+++ ext_emconf.php
@@ -4,15 +4,15 @@ $EM_CONF[$_EXTKEY] = [
     'title' => 'osm - OpenStreetMap',
     'description' => 'A smart OpenStreetMap solution in TYPO3',
     'category' => 'plugin',
-    'version' => '4.0.3',
+    'version' => '13.0.0',
     'author' => 'Alex Kellner',
     'author_email' => 'alexander.kellner@in2code.de',
     'author_company' => 'in2code.de',
     'state' => 'stable',
     'constraints' => [
         'depends' => [
-            'typo3' => '12.4.0 - 12.4.99',
-            'php' => '8.1.0 - 8.2.99',
+            'typo3' => '13.4.0 - 13.4.99',
+            'php' => '8.2.0 - 8.3.99',
         ],
         'conflicts' => [
         ],
